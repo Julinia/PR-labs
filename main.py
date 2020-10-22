d8ccbf90f0d3a851946fe660d40ef2cff09d4af1
Turcanu Iuliana <iuliana.turcanu@sens.media>
2020-10-22 08:34:55 +0300

    fetch routes

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import requests
+import concurrent.futures
 
 def load_url (url, token):
     return requests.get('http://localhost:5000' + url, headers={'X-Access-Token': token}).json()
 
 access_token = load_url('/register', '')['access_token']
 access_home = load_url('/home', access_token)['link']
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    links = access_home
+    future_to_url = {executor.submit(load_url, links[key], access_token): key for key in links}
+    for future in concurrent.futures.as_completed(future_to_url):
+        key = future_to_url[future]
+        data = future.result()
+        print(data)
